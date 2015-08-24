fc1fb73cec96e68a9a2b9e7ca9d83c7d3abe9d93
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-08-24 12:12:22 -0400

    Update __init__.py

--- imagehash/__init__.py
+++ imagehash/__init__.py
@@ -1,7 +1,6 @@
 '''An image hash module'''
 from __future__ import absolute_import
 
-from .imagehash import (grayscale_hash, ImageHash, hex_to_hash,
-                        color_hash)
+from .imagehash import (grayscale_hash, ImageHash, hex_to_hash, color_hash)
 
-__all__ = ['average_hash', 'get_pixels']
+__all__ = ['color_hash', 'grayscale_hash', 'hex_to_hash', 'ImageHash']
