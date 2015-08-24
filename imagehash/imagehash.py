46eaf6106f0bf376e1a8a79bb11a24a0d3195f6b
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-08-24 00:16:32 -0400

    Moved some things around

--- imagehash/imagehash.py
+++ imagehash/imagehash.py
@@ -1,18 +1,6 @@
 from PIL import Image
 import numpy
 
-def average_hash(image, hash_size):
-    '''Take the average of the image'''
-    image = image.convert("L").resize((hash_size, hash_size), Image.ANTIALIAS)
-    pixels = numpy.array(image.getdata()).reshape((hash_size, hash_size))
-    avg = pixels.mean()
-    diff = pixels > avg
-    return diff
-
-def get_pixels(image, hash_size):
-    image = image.convert("RGB").resize((hash_size, hash_size), Image.ANTIALIAS)
-    pixels = numpy.array(image.getdata()).flatten('F').reshape((3, hash_size, hash_size))
-    return pixels
 
 def split_every_n(line, n):
     return [line[i:i+n] for i in range(0, len(line), n)]
@@ -29,6 +17,19 @@ class ImageHash(object):
 
     def __sub__(self, other):
         return numpy.absolute(self.array.flatten() - other.array.flatten()).sum()
+
+def average_hash(image, hash_size=8):
+    '''Take the average of the image'''
+    image = image.convert("L").resize((hash_size, hash_size), Image.ANTIALIAS)
+    pixels = numpy.array(image.getdata()).reshape((hash_size, hash_size))
+    avg = pixels.mean()
+    diff = pixels > avg
+    return ImageHash(diff)
+
+def color_hash(image, hash_size=8):
+    image = image.convert("RGB").resize((hash_size, hash_size), Image.ANTIALIAS)
+    pixels = numpy.array(image.getdata()).flatten('F').reshape((3, hash_size, hash_size))
+    return ImageHash(pixels)
 
 def covert_hex_to_image_hash(hex_string):
     array = numpy.array([int(num, 16) for num in split_every_n(hex_string, 2)])
