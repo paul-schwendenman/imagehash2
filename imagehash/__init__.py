718fd8c3c6b58cba2eed42f47f93d5b139a3d522
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-08-24 00:32:57 -0400

    Setup demo to show new color_hash

--- imagehash/__init__.py
+++ imagehash/__init__.py
@@ -1,7 +1,7 @@
 '''An image hash module'''
 from __future__ import absolute_import
 
-from .imagehash import (average_hash, get_pixels, ImageHash,
-                        covert_hex_to_image_hash)
+from .imagehash import (average_hash, ImageHash, covert_hex_to_image_hash,
+                        color_hash)
 
 __all__ = ['average_hash', 'get_pixels']
